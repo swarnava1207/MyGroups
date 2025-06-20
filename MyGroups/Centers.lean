148f90d7ed58a52d22170abe4a3bae56519e3d60
swarnava1207 <swarnavac@iisc.ac.in>
2025-06-20 11:52:36 +0530

    Changes made

--- MyGroups/Centers.lean
+++ MyGroups/Centers.lean
@@ -54,13 +54,158 @@ theorem center_subgroup (G : Type u) [MyGroup G] :
       rw [← inv_of_inv a, ← inv_of_prod h a⁻¹]
       rw [← hh a⁻¹, inv_of_prod]
 
-instance {G : Type u} [MyGroup G] : HMul G (Set G) (Set G) where
+instance left_mul_set {G : Type u} [MyGroup G] : HMul G (Set G) (Set G) where
   hMul := fun g H => { g * a | a ∈ H}
 
-instance {G : Type u} [MyGroup G] : HMul (Set G) G (Set G) where
+instance right_mul_set {G : Type u} [MyGroup G] : HMul (Set G) G (Set G) where
   hMul := fun H g => { a * g | a ∈ H}
+
+theorem normal_assoc {G : Type u} [MyGroup G] (g h : G) (H : Set G) :
+  g * (H * h) = (g * H) * h := by
+    apply Set.ext
+    intro x
+    constructor
+    · intro hx
+      simp [left_mul_set] at hx
+      let ⟨ a , ha ⟩ := hx
+      simp [right_mul_set] at ha
+      let ⟨ b , hb ⟩ := ha.1
+      use g * b
+      constructor
+      · use b
+        constructor
+        · apply hb.1
+        · rfl
+      · rw [assoc, hb.2]
+        rw [ha.2]
+    · intro hx
+      simp [right_mul_set] at hx
+      let ⟨ a , ha ⟩ := hx
+      simp [left_mul_set] at ha
+      let ⟨ b , hb ⟩ := ha.1
+      use b * h
+      constructor
+      · use b
+        constructor
+        · apply hb.1
+        · rfl
+      · rw [← assoc, hb.2]
+        rw [ha.2]
+
+theorem left_mul_assoc {G : Type u} [MyGroup G] (g h : G) (H : Set G) :
+  g * (h * H) = (g * h) * H := by
+    apply Set.ext
+    intro x
+    constructor
+    · intro hx
+      simp [left_mul_set] at hx
+      let ⟨ a , ha ⟩ := hx
+      use a
+      simp [left_mul_set]
+      assumption
+    · intro hx
+      simp [left_mul_set] at hx
+      let ⟨ a , ha ⟩ := hx
+      use h * a
+      simp [left_mul_set]
+      constructor
+      · use a
+        constructor
+        · apply ha.1
+        · rfl
+      · apply ha.2
+
+theorem right_mul_assoc {G : Type u} [MyGroup G] (H : Set G) (g h : G) :
+  (H * g) * h = H * (g * h) := by
+    apply Set.ext
+    intro x
+    constructor
+    · intro hx
+      simp [right_mul_set] at hx
+      let ⟨ a , ha ⟩ := hx
+      use a
+    · intro hx
+      simp [right_mul_set] at hx
+      let ⟨ a , ha ⟩ := hx
+      use a * g
+      simp [right_mul_set]
+      constructor
+      · use a
+        constructor
+        · apply ha.1
+        · rfl
+      · apply ha.2
 
 def normalizer (G : Type u) [MyGroup G] (H : Set G) : Set G :=
   { g : G | g * H = H * g }
 
+theorem normalizer_subgroup (G : Type u) [MyGroup G] (H : Set G) :
+  SubGroup G (normalizer G H) := by
+    constructor
+    · simp [normalizer]
+      apply Set.ext
+      intro x
+      constructor
+      · intro h
+        simp [right_mul_set]
+        simp [left_mul_set] at h
+        assumption
+      · intro h
+        simp [left_mul_set]
+        simp [right_mul_set] at h
+        assumption
+    · intro a ha
+      simp [normalizer] at ha
+      simp [normalizer]
+      apply Set.ext
+      simp [Set.ext_iff] at ha
+      simp [left_mul_set, right_mul_set] at ha
+      intro x
+      constructor
+      · intro h
+        simp [right_mul_set]
+        simp [left_mul_set] at h
+        let ⟨ a₃ , ha₃ ⟩ := h
+        let ⟨ h₁ , h₂ ⟩ := ha (a * x * a)
+        have hsub : ∃ a₃ ∈ H , a₃ * a = a * x * a := by
+          use a₃
+          constructor
+          · apply ha₃.1
+          · rw [← ha₃.2]
+            rw [← assoc a a⁻¹ a₃]
+            simp
+        let ⟨ a₂ , ha₂ ⟩ := h₂ hsub
+        use a₂
+        constructor
+        · apply ha₂.1
+        · simp [cancel_left] at ha₂
+          rw [ha₂.2]
+          simp
+      · intro h
+        simp [left_mul_set]
+        simp [right_mul_set] at h
+        let ⟨ a₃ , ha₃ ⟩ := h
+        let ⟨ h₁ , h₂ ⟩ := ha (a * x * a)
+        have hsub : ∃ a₃ ∈ H , a * a₃ = a * x * a := by
+          use a₃
+          constructor
+          · apply ha₃.1
+          · rw [← ha₃.2]
+            rw [← assoc a a₃ a⁻¹]
+            simp
+        let ⟨ a₂ , ha₂ ⟩ := h₁ hsub
+        use a₂
+        constructor
+        · apply ha₂.1
+        · rw [cancel_right a a₂ (a * x)] at ha₂
+          rw [ha₂.2, ← assoc]
+          simp
+    · intro a b ha hb
+      simp [normalizer] at ha hb
+      simp [normalizer]
+      rw [← right_mul_assoc]
+      rw [← ha]
+      rw [← normal_assoc]
+      rw [← hb]
+      rw [left_mul_assoc]
 
